d4fc634d4a4ef684a233f532cb7485f253e0e21b
Ramon Pilon <ramonbrx98@gmail.com>
2023-01-21 19:22:49 -0300

    up

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,7 @@
+let country = 'Brasil';
+let continent = 'South-America';
+let population = 243455545;
+let isIsland = false;
+let language 
+
+console.log(country,continent,population,isIsland,language)
