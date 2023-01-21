c59ce176e20282887267be7a2e2d957b79042275
Ramon Pilon <ramonbrx98@gmail.com>
2023-01-21 20:51:10 -0300

    up

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -5,3 +5,26 @@ let isIsland = false;
 let language 
 
 console.log(country,continent,population,isIsland,language)
+
+let Markmass = 78;
+let MarkHeight = 1.88;
+let Johnmass = 85;
+let JohnHeight = 1.76;
+
+let BMIMark = Markmass / MarkHeight ** 2; 
+let BMIJohn = Johnmass / JohnHeight ** 2;
+
+const markHigherBMI = BMIMark > BMIJohn;
+
+if (markHigherBMI != false) {
+    console.log("Mark's BMI is higher than John's!")
+} else {
+    console.log("John's BMI is higher than Mark's!")
+}
+
+
+
+console.log(`John
+is
+a
+father`);
