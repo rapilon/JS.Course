521cb77f833f363e2c8f94ec3d229bee9e96f630
Ramon Pilon <ramonbrx98@gmail.com>
2023-01-25 18:55:14 -0300

    up

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,55 @@
 'use strict';
+
+// console.log(document.querySelector('.message'));
+// document.querySelector('.message').textContent = 'Correct number';
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+// document.querySelector('.guess').value = 23;
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+//When wanna play again
+document.querySelector('.again').addEventListener('click', function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  displayMessage('Start guessing...');
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').textContent = '';
+});
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+  //when there is no input
+  if (!guess) {
+    displayMessage('⛔No number!');
+    //when players wins
+  }else if (guess === secretNumber) {
+    displayMessage('🎉Correct Number!');
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📈Too high!' : '📉Too low!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    }
+  } else {
+    displayMessage('💥You lost the game!');
+    document.querySelector('.score').textContent = 0;
+  }
+});
