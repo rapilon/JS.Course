93b39a5b1a45edc0cd0739949c8bd8092048fdaa
Ramon Pilon <ramonbrx98@gmail.com>
2023-01-30 21:54:15 -0300

    up

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+function calcAge(birthYear) {
+    const age = 2037 - birthYear;
+    function printAge() {
+        const output = `${firstName}, you are ${age}, born in ${birthYear}`;
+        console.log(output);
+
+        if(birthYear >= 1981 && birthYear <= 1996) {
+            const str = `Oh, and you're a millenial, ${firstName}`;
+            console.log(str);
+        }
+    }
+    printAge();
+    return age;
+}
+
+const firstName = 'Ramon';
+calcAge(1998);
+//console.log(age); erro
+//printAge(); erro
+console.log(this);
